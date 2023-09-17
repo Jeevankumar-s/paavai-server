f15d3e649e2b48b11dd01f10573c3a9ae821f643
Jeevankumar-s <jeevenkumar2003@gmail.com>
2023-09-17 22:22:54 +0530

    added users

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const { Client } = require('pg');
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 const app = express();
 app.use(express.json());
@@ -16,29 +18,48 @@ client
   .connect()
   .then(() => {
     console.log('Connected to the database');
-    // Create the "users" table.
+    // Create the "login" table if it doesn't exist.
     const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS kumar (
+      CREATE TABLE IF NOT EXISTS login (
         id serial PRIMARY KEY,
         username VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL
+        password VARCHAR(255) NOT NULL,
+        type VARCHAR(255) NOT NULL
       );
     `;
 
     return client.query(createTableQuery);
   })
   .then(() => {
-    console.log('Table "users" created successfully');
-    // Perform other database operations here.
+    console.log('Table "login" created or already exists');
   })
   .catch((error) => {
     console.error('Error:', error);
   });
 
-app.get('/users', (req, res) => {
-  res.send({ jeev: "vanakkam" });
-  console.log('hiiii')
+app.post('/login', async(req, res) => {
+  const {username,password,type}=req.body;
+  const insertQuery = `INSERT INTO login (username, password, type) VALUES ($1, $2, $3) RETURNING *`;
+  const hashedPassword=await bcrypt.hash(password,5)
+  client
+    .query(insertQuery, [username, hashedPassword, type])
+    .then((result) => {
+      const insertedUser = result.rows[0];
+      res.status(201).json(insertedUser);
+      console.log('success')
+    })
+    .catch((error) => {
+      console.error('Error inserting user:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
+
+app.get('/users',async(req,res)=>{
+  const getUser=`select * from login;`;
+  console.log(await client.query(getUser));
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
