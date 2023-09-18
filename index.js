5a8178ec88f0881fddc732f3ab358ad3d20a97f6
Jeevankumar-s <jeevenkumar2003@gmail.com>
2023-09-18 11:51:14 +0530

    commit by Jeevan

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const { Client } = require('pg');
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
+const cors = require('cors'); 
 const app = express();
 app.use(express.json());
+app.use(cors()); 
 const PORT = process.env.PORT || 3303;
 
 const connectionString = process.env.URL; // Replace with your actual ElephantSQL database URL.
