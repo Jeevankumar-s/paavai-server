e5164c8a01774f5665b7995037502a7086ea8a13
Jeevankumar-s <jeevenkumar2003@gmail.com>
2023-09-18 11:43:48 +0530

    commit by Jeevan

--- index.js
+++ index.js
@@ -68,9 +68,9 @@ const JWT_SECRET = process.env.JWT_SECRET
     const passwordMatch = await bcrypt.compare(password, storedHashedPassword);
           const user=result.rows
        const userType=user[0].type
-       res.send(userType)
-      // const jwtToken = await jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' });
-      // res.send(jwtToken)
+      //  res.send(userType)
+      const jwtToken = await jwt.sign({ username }, "JEEVANKUMAR", { expiresIn: '1h' });
+      res.send(jwtToken)
 
       // res.status(201).send({ jeevToken: jwtToken,userType:userType,validation:true});
 
