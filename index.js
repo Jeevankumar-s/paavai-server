8913667d35c1fe5e21a601085ea7b99572ff8da7
Jeevankumar-s <jeevenkumar2003@gmail.com>
2023-09-21 14:46:07 +0530

    added connection between frontend gmail

--- index.js
+++ index.js
@@ -65,8 +65,8 @@ client
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: process.env.GMAIL,
-      pass: process.env.GMAIL_PASS,
+      user:"paavaioutpass@gmail.com",
+      pass: "iiua xnjs iwny krmg",
     },
   });
 
